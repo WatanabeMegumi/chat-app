cd62c573a88b8a55aa1817baf57e71300b32b2be
WatanabeMegumi <wmegumi001@gmail.com>
2025-02-18 16:59:03 +0900

    ユーザー認証機能の実装

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,7 @@
 
 ActiveRecord::Schema[7.1].define(version: 2025_02_18_065200) do
   create_table "users", charset: "utf8mb3", force: :cascade do |t|
+    t.string "name", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
